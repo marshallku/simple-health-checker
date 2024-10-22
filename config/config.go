be93e6ef46982f7dd9c6c3d091e27fbb79b07bb4
Marshall Ku <marshall@kakao.com>
2024-10-22 11:20:59 +0900

    Add config file parser

---
+++ config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"bytes"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	WebhookURL string `yaml:"webhook_url"`
+	Timeout    int    `yaml:"timeout"`
+	Pages      []Page `yaml:"pages"`
+}
+
+type Page struct {
+	URL           string   `yaml:"url"`
+	Status        int      `yaml:"status"`
+	TextToInclude string   `yaml:"text_to_include"`
+	Speed         int      `yaml:"speed"`
+	Request       *Request `yaml:"request,omitempty"`
+}
+
+type Request struct {
+	Method  string            `yaml:"method"`
+	Headers map[string]string `yaml:"headers"`
+	Body    string            `yaml:"body"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(file)
+	data := buf.Bytes()
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/marshallku/simple_health_checker/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	configFile := flag.String("config", "config.yaml", "Path to configuration file")
+	flag.Parse()
+
+	var err error
+	var cfg *config.Config
+	cfg, err = config.LoadConfig(*configFile)
+
+	if err != nil {
+		fmt.Printf("Error loading configuration: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Loaded configuration: %+v\n", cfg)
+
 }
